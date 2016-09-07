2be9b1c641c32a583d2f2f84b47cf6733acc64e7
Kevin Kwan <KevinKwan95@googlemail.com>
2016-09-07 02:15:33 +0100

    Refactor result formatting for more customisability.

--- tasks/sass-lint.js
+++ tasks/sass-lint.js
@@ -1,13 +1,53 @@
 'use strict';
 const lint = require('sass-lint');
+const path = require('path');
+
+function getFormatter(format) {
+	let formatterPath;
+
+	// default is stylish
+	format = format || 'stylish';
+
+	// only strings are valid formatters
+	if (typeof format === 'string') {
+		// replace \ with / for Windows compatibility
+		format = format.replace(/\\/g, '/');
+
+		// if there's a slash, then it's a file
+		if (format.indexOf('/') > -1) {
+			formatterPath = path.resolve(process.cwd(), format);
+		}
+		else {
+			formatterPath = `eslint/lib/formatters/${format}`;
+		}
+
+		try {
+			return require(formatterPath);
+		}
+		catch (ex) {
+			ex.message = `There was a problem loading formatter: ${formatterPath}\nError: ${ex.message}`;
+			throw ex;
+		}
+	}
+	else {
+		return null;
+	}
+}
 
 module.exports = (grunt) => {
 	grunt.verbose.writeln(`\n${lint.info}\n`);
 
 	grunt.registerMultiTask('sasslint', 'Lint your Sass', function () {
 		const options = this.options({
-			configFile: ''
+			configFile: '',
+			format: 'stylish'
 		});
+
+		const formatter = getFormatter(options.format);
+
+		if (!formatter) {
+			grunt.fail.warn(`Could not find formatter ${options.format}`);
+		}
 
 		let results = [];
 		let fileCount = 0;
@@ -25,7 +65,7 @@ module.exports = (grunt) => {
 		let warningMessage = '';
 
 		if (resultCount > 0) {
-			const resultFormat = lint.format(results, { options: options });
+			const resultFormat = formatter(results);
 
 			// Log linting output.
 			if (options.outputFile) {
